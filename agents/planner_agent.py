786d85bcdc0d2c588470586e0df09b4f7d6d987c
Joy Patel <joypatel3618@gmail.com>
2026-02-20 20:24:36 +0530

    Added DAG scheduling

--- agents/planner_agent.py
+++ agents/planner_agent.py
@@ -3,14 +3,14 @@ from core.task import Task
 
 class PlannerAgent(BaseAgent):
     def plan(self, goal):
-        self.logger.info(
-            f"Planning Tasks for goal : {goal}"
-            )
-        tasks =[ 
-            Task(description=f"{goal} - Step 1"),
-            Task(description=f"{goal} - Step 2"),
-            Task(description=f"{goal} - Step 3"),            
-        ]
-        return tasks
+        self.logger.info(f"Planning Tasks for goal : {goal}")
+        t1 = Task(description=f"{goal} - Step 1")
+        t2 = Task(description=f"{goal} - Step 2")
+        t3 = Task(description=f"{goal} - Step 3")
+
+        t2.context["depends_on"]=[t1.id]
+        t3.context["depends_on"]=[t2.id]
+        
+        return [t1,t2,t3]
     def run(self, task):
         raise NotImplementedError("PlannerAgent doest not execute tasks. Use plan() instead")
--- core/scheduler.py
+++ core/scheduler.py
@@ -7,6 +7,13 @@ class Scheduler:
         self.executor=executor
         self.supervisor=supervisor
         self.poll_interval=poll_interval
+        self.completed_tasks=set()
+    
+    def dependency_resolved(self,task):
+        deps = task.context.get("depends_on",[])
+        if not deps:
+            return True
+        return all(dep_id in self.completed_tasks for dep_id in deps)
 
     def run(self):
         while True:
@@ -14,7 +21,16 @@ class Scheduler:
             if task is None:
                 time.sleep(self.poll_interval)
                 continue
+            if not self.dependency_resolved(task):
+                self.task_queue.enqueue(task)
+                time.sleep(self.poll_interval)
+                continue
+
             self.executor.run(task)
             self.supervisor.run(task)
+            
+            if task.state == TaskState.COMPLETED:
+                self.completed_tasks.add(task.id)
+
             if task.state == TaskState.PENDING:
                 self.task_queue.enqueue(task)
